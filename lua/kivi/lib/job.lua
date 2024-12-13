bdd790fc23ca23df970fed1d49a04139d3c840a3
notomo <notomo.motono@gmail.com>
2024-12-13 20:39:13 +0900

    Small refactor

--- lua/kivi/lib/job.lua
+++ lua/kivi/lib/job.lua
@@ -4,24 +4,18 @@ function M.promise(cmd)
   local promise, resolve, reject = require("kivi.vendor.promise").with_resolvers()
 
   local ok, err = pcall(function()
-    vim.system(cmd, {
-      stdout = function(_, data)
-        if not data then
-          return
+    vim.system(
+      cmd,
+      {
+        text = true,
+      },
+      vim.schedule_wrap(function(o)
+        if o.code == 0 then
+          return resolve(vim.trim(o.stderr .. o.stdout))
         end
-        vim.schedule(function()
-          resolve(data)
-        end)
-      end,
-      stderr = function(_, data)
-        if not data then
-          return
-        end
-        vim.schedule(function()
-          reject(data)
-        end)
-      end,
-    })
+        return reject(vim.trim(o.stdout .. o.stderr))
+      end)
+    )
   end)
   if not ok and err then
     reject(err)
